16f82e1604c1cec5081fc1da207e132d50592ab4
kusano <kusano.hatsumi@gmail.com>
2023-06-01 15:16:05 +0900

    メインページ作成

--- src/App.js
+++ src/App.js
@@ -1,4 +1,30 @@
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { Main } from "./main/Main";
+
 function App() {
-  return <></>;
+  const [state, setState] = useState("");
+  useEffect(() => {
+    setState(false);
+    setTimeout(() => {
+      console.log("loading end");
+      setState(true);
+    }, 4700);
+  }, []);
+  console.log(state);
+  return (
+    <>
+      {/* <Header /> */}
+      <main className="App">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/* <Route path="/work" element={<Works />} />
+          <Route path="/works/:itemId" element={<Work />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </>
+  );
 }
 export default App;
